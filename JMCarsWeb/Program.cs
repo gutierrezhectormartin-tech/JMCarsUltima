164903c5e7abfb4f8cc01776aeb706e021e7f3bd
jgalatiuy <jgalati.jg@gmail.com>
2026-06-11 14:49:02 -0300

    AHORA SI!!!!!!!!!!!!!!!!!

--- JMCarsWeb/Program.cs
+++ JMCarsWeb/Program.cs
@@ -5,6 +5,7 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+//A ver ahora si agarra el cambio
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
